024cf4852acc009693d78e5ae35ddde099af7abf
Nathaniel H Clay <nclay1@mix.wvu.edu>
2010-10-06 14:16:29 +0100

    Fixed Formatting

--- src/mpx_r2.c
+++ src/mpx_r2.c
@@ -262,75 +262,76 @@ else{
 
 /** This is a user function in the menu that puts a process in the ready state if previously blocked and blocked if previously suspended it takes the process name as input*/
 void resume(char name[STRLEN]){
-PCB tempPCB=mpx_r2.find_PCB(name); ///< finds the pcb
+	PCB tempPCB = find_PCB(name); ///< finds the pcb
 
-if(tempPCB==null){ ///< checks to make sure the PCB was found
-printf("The process could not be found");
-}
-else{
-if(tempPCB.state==0x08){ ///< if blocked makes it ready
-tempPCB->state=0x04;
-}
-else if(tempPCB.state==0xFF){ ///< if suspended makes it blocked
-tempPCB->state=0x0;
-}
-}
+	if(tempPCB==null){ ///< checks to make sure the PCB was found
+		printf("The process could not be found");
+	}
+	else{
+			if(tempPCB.state==0x08){ ///< if blocked makes it ready
+				tempPCB->state=0x04;
+			}
+			else if(tempPCB.state==0xFF){ ///< if suspended makes it blocked
+				tempPCB->state=0x0;
+			}
+	}
 }
 
 /** This is a user function from the menu it changes the priority of a PCB and takes the name and desired priority as inputs80ij*/
 void setPriority(char name [STRLEN],signed char priority){
-PCB tempPCB=mpx_r2.find_PCB(name);
+	PCB tempPCB= find_PCB(name);
 
-if((tempPCB==null)||(127<=priority<=-128)){ ///< checks for validity
-printf("The process could not be found");
-}
-else{
-mpx_r2.remove(tempPCB);
-mpx_r2.insert(tempPCB);
-}
+	if((tempPCB==null)||(127<=priority<=-128)){ ///< checks for validity
+		printf("The process could not be found");
+	}
+	else{
+		remove(tempPCB);// What the hell
+		insert(tempPCB);
+	}
 
 }
 
 /** This is a user command from the menu it is used to show information about a specific PCB*/
 void showPCB(char name [STRLEN]){
-PCB tempPCB=mpx_r2.find_PCB(name);
-if(tempPCB==null){ ///< checks to make sure the PCB was found
-printf("The process could not be found");
-}
-else{
-printf("Process name: %s \n State of process: %s \n",name,tempPCB.state);
-}
+	PCB tempPCB= find_PCB(name);
+    
+	if(tempPCB==null){ ///< checks to make sure the PCB was found
+		printf("The process could not be found");
+	}
+	else{
+		printf("Process name: %s \n State of process: %s \n",name,tempPCB.state);
+	}
 }
 
 /** This is a user functions that shows name and state of all processes */
 void showAll(){ // Pagination function needs added !!Function still needs work!!
-PCB tempPCB=mpx_r2.getHead();
-while(tempPCB.next!=null){ ///< cycles through each PCB in the linked list and prints out name and state
-PCB tempPCB=tempPCB.next;
-printf("%s %s \n",name,tempPCB.state);
-}
+		PCB tempPCB=mpx_r2.getHead(); // FIX ME: GOD DAMMIT
+	while(tempPCB.next!=null){ ///< cycles through each PCB in the linked list and prints out name and state
+		PCB tempPCB=tempPCB.next;
+		printf("%s %s \n",name,tempPCB.state);
+	}
 }
 
 /** This is a user function that shows all non-suspended processes followed by suspended processes */
 void showReady(){ // Pagination function needs added !!Function still needs work!!
-PCB tempPCB=mpx_r2.getHead();
-while(tempPCB.next!=null){
-PCB tempPCB=tempPCB.next;
-if(tempPCB.state==ready)
-printf("%s,%s,%s \n",name,priority,status);
-}
-}
+	PCB tempPCB=mpx_r2.getHead(); //FIX ME: What the hell
+	while(tempPCB.next!=null){
+		PCB tempPCB=tempPCB.next;
+		if(tempPCB.state==ready)
+			printf("%s,%s,%s \n",name,priority,status);
+		}
+	}
 }
 
 /** This is a user function that shows all blocked processes followed by non-blocked processes */
 void showBlocked(){ // Pagination function needs added !!Function still needs work!!
-PCB tempPCB=mpx_r2.getHead();
-while(tempPCB.next!=null){
-PCB tempPCB=tempPCB.next;
-if(tempPCB.state==ready)
-printf("%s,%s,%s \n",name,priority,status);
-}
-}
+	PCB tempPCB=mpx_r2.getHead();
+	while(tempPCB.next!=null){
+	PCB tempPCB=tempPCB.next;
+	if(tempPCB.state==ready)
+		printf("%s,%s,%s \n",name,priority,status);
+	}
+}	
 }
 
 
