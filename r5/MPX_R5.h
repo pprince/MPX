9329c75fd456a30be15ee16f2dd3e8ad43148c2b
nick <nyanak@mix.wvu.edu>
2010-10-31 23:24:10 +0000

    more work on r5

--- r5/MPX_R5.h
+++ r5/MPX_R5.h
@@ -1,16 +1,38 @@
-	# define COM1 0x3F8 
-	#define PIC_MASK 0x21
+	# define COM1BASE 0x3F8  // also LSB
+	# define INTID 0x0C // recheck this
+	# define INTEN COM1BASE+1  // also MSB
+	# define INTID_REG COM1BASE+2 
+	# define LC COM1BASE+3 
+	# define MC COM1BASE+4 
+	# define LS COM1BASE+5 
+	# define MS COM1BASE+6 
+	
+	# define PIC_MASK 0x21
+	# define EOI 0x20
+	# define OPEN 0
+	# define CLOSED 1
+	# define AVAILABLE 0
+	# define IN_USE 1
+	# define READ 0
+	# define WRITE 1
+	# define IDLE 2
+	
 	
 	typedef struct deviceControlBlock{
-		status,current,eventFlag,Ubuffer,Ibuffer,
-		requested,transferred,temp,
+		char ringBugger[1000];
+		int ringSize,get,put;
+		
+		int ocFlag,status,currentOp,requested,transferred;
+		char* Ibuff,Obuff;
+		
 	}DCB;
 		
 	//prototypes
-	int com_open (int *eflag_p, int baud_rate);
-	int com_close (void);
-	int com_read (char *buf_p,int *count_p);
-	int com_write (char *buf_p,int *count_p);
+	int com_open(int *, int ); 
+	int com_close(void); 
+	int com_read(char *, int *); 
+	int com_write(char *, int *); 
+	
 	void setvect(int int_ID,void interrupt(*handler)(void));
 	void interrupt(*getvect(int int_ID))(void);
 	void disable(void);
