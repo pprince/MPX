762227fb3e386b3867b257c72fa5748b404480fd
nick <nyanak@mix.wvu.edu>
2010-11-11 16:34:24 +0000

    completer first draft of r5 (i think) going to look it over again and fix

--- r5/MPX_R5.c
+++ r5/MPX_R5.c
@@ -5,24 +5,64 @@
 	DCB dcb;
 
 	int com_open (int *eflag_p, int baud_rate){
-		if(((eflag_p||baud_rate)==NULL)||(baud_rate<=0)||(dcb->ocFlag==OPEN)){
+		//check for closed,null,invalid, set variables , send values to ports, disable send 0x31 enable
+ 		long brd;
+    	int mask;
+		
+		if((dcb->ocFlag==OPEN)&&(eflag_p!=NULL)&&(baud_rate<=0)){
 			return;
 		}
 		
-	baud_rate_div = 115200 / (long) baud_rate
+		dcb->status=IDLE;
+		dcb->ocFlag=OPEN;
+		dcb->ocFlagPtr=eflag_p;
+		dcb->count=0;
+		dcb->Ibuff=0;
+		dcb->Obuff=0;
+		
+	  	oldfunc = getvect(INTID); 
+	    setvect(INDID, & level1);     
+	    brd=115200/(long)baud_rate; 
+	    outportb(LC, 0x80); // set to 1000 1000 
+	    outportb(COM1BASE, brd & 0xFF); // xxxx xxxx and 1111 1111 = xxxx xxxx
+	    //check this why not just make it brd?
+	    outportb(INTEN,(brd>>8) & 0xFF); // shift xxxx xxx to the right 8 times then and 1111 1111
+	    //check this why not just shift it to the right eight and wouldnt that put it back to its original?
+	    outportb(LC, 0x03); // set to 0000 0011
+ 
 
-	...
-	int mask;
-	...
-	disable();
-	mask = inportb(PIC_MASK);
-	mask = mask & ~0x80;
-	outportb(PIC_MASK, mask);
-	enable();
+		disable(); 
+		mask = inportb(PIC_MASK); 
+		mask = mask | 0x10; //0010 0001 or 0001 0000 = 0011 0001 = 0x31
+		outportb(PIC_MASK, mask);
+		enable(); 
+		
+		outportb(INTEN, 0x01); // set to 0000 0001
+  		outportb(MC, 0x08); // set to 0000 1000
+  		
 	
 	}
 	
-	int com_close (void){}
+	int com_close (void){
+		// check if open, disable send 0x31 enable, set stuff to 0x00
+ 		int mask; 
+		
+		if(dcb->ocFlag!=OPEN) 
+	        return; 
+	         
+	    dcb->ocFlag=CLOSED; 
+	    
+	    disable(); 
+	    mask = inportb(PIC_MASK); 
+	    mask = mask | 0x10; //0010 0001 or 0001 0000 = 0011 0001 = 0x31
+	    outportb(PIC_MASK, mask); 
+	    enable(); 
+	    
+	    outportb(INTEN,0x00);      
+	    outportb(MS,0x00); 
+	    setvect(INTID,oldfunc); 
+    
+	}
 	
 	void interrupt level1(){
 		//check if open, check to find out what lvl 2 interrupt then call
@@ -33,7 +73,7 @@
 		}
 		
 		else{
-			temp=inportb(INTID_REG&&0x07); // 0011 1111 1001 and 0000 0111
+			temp=inportb(INTID_REG&&0x07); // 0011 1111 1001 and 0000 0111 = 0000 0001 = 0x01
 			if(temp==2){
 				level2Write();
 			}
@@ -63,7 +103,8 @@
 			*dcb->ocFlag=OPEN;
 	
 			mask = inportb(INTEN);
-			mask = mask&~0x02;  //0000 1101 and 0000 0010
+			mask = mask & 0x02;  //0000 1101 and 0000 0010 = 0000 0000 = 0x00 
+			//why not name this mask = 0 check this?
 			outportb(INTEN, mask);
 		}
 			
@@ -145,7 +186,7 @@
 		dcb->trans++;
 		dcb->Ibuff++;
   		mask = inportb(INTEN); 
-	 	mask = mask | 0x02;  // 0000 1101 or 0000 0010
+	 	mask = mask | 0x02;  // 0000 1101 or 0000 0010 = 0000 1111 = 0x0F
 	 	
 	 	outportb(INTEN,mask); 
 	}
--- r5/MPX_R5.h
+++ r5/MPX_R5.h
@@ -33,12 +33,8 @@
 	int com_close(void); 
 	int com_read(char *, int *); 
 	int com_write(char *, int *); 
+	void level2Write();
+	void level2Read();
+	void interrupt level1(void);
+	void interrupt (*oldfunc) (void);
 	
-	void setvect(int int_ID,void interrupt(*handler)(void));
-	void interrupt(*getvect(int int_ID))(void);
-	void disable(void);
-	void enable(void);
-	void outportb (int port_id, unsigned char value);
-	unsigned char inportb (int port_id);
-	
-
